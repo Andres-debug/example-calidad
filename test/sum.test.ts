3d41b4e567b2c96e4cb4fb04dd614aa10d4537f9
andresf.diazm@unac.edu.co <andresf.diazm@unac.edu.co>
2022-03-30 19:18:51 -0500

    First test

---
+++ test/sum.test.ts
@@ -0,0 +1,15 @@
+import {sum} from '../src/sum';
+
+// test('Test sum function',()=>{
+//     it('Should return 15 for sum(10,5)',()=>{
+//         expect(sum(10,5)).toBe(15);
+//     });
+//     it('Should return 5 for sum(2,3)',()=>{
+//         expect(sum(2,3)).toBe(5);
+//     })
+// });
+
+test('Should return 15 for sum(10,5)',()=>{
+        expect(sum(10,5)).toBe(15);
+ 
+});
